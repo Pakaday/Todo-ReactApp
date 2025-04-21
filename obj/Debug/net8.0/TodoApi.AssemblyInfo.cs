56e32618d52fbc3467e9b86ca7e3402c3904cf94
Frederick Neimeister <pakaday@gmail.com>
2025-04-21 19:03:03 -0400

    npm run build for .env change

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06d1c1e723d977a449fd8f3926968bf0479c5ad1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f832d4c877daabaacc527d1a396af16ac8f0a36")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
