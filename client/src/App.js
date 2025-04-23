5a644638f79c62271c1ab3771efa03ffc326e021
Frederick Neimeister <pakaday@gmail.com>
2025-04-23 10:14:51 -0400

    Add missing / Line 79 TodoItems/${id}

--- client/src/App.js
+++ client/src/App.js
@@ -76,7 +76,7 @@ function App() {
     const handleDelete = (id) => {
         console.log('Deleting ID:', id);
         //fetch(`http://localhost:5209/api/TodoItems/${id}`, {
-        fetch(`${process.env.REACT_APP_API_URL}/TodoItems${id}`, {
+        fetch(`${process.env.REACT_APP_API_URL}/TodoItems/${id}`, {
 
             method: 'DELETE'
         })
