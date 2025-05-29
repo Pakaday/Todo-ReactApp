4d4adcafbfbb062451347a09bfa223af846faf29
Frederick Neimeister <pakaday@gmail.com>
2025-05-29 16:28:41 -0400

    Added DTO

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Data;
+using TodoApi.DTO;
 using TodoApi.Models;
 
 namespace TodoApi.Controllers
@@ -23,6 +24,26 @@ namespace TodoApi.Controllers
 			_context.Users.Add(user);
 			await _context.SaveChangesAsync();
 			return Ok(user);
+		}
+
+		// User login
+		[HttpPost("login")]
+		public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
+		{
+			var user = await _context.Users.SingleOrDefaultAsync(u => u.Username ==  loginDTO.Username);
+			if (user == null)
+			{
+				return Unauthorized("Invalid username or password");
+			}
+
+			// For testing REMOVE 
+			if (user.PasswordHash != loginDTO.Password)
+			{
+				return Unauthorized("Invalid username or password");
+			}
+
+			// Return User ID and Username
+			return Ok(new { user.Id, user.Username });
 		}
 
 		// Get all users
