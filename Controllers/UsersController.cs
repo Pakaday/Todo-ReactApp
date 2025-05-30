45464e44f0a2678b9302d795c127a45740a76d63
Frederick Neimeister <pakaday@gmail.com>
2025-05-30 15:41:58 -0400

    Working authentication for multiple users

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -3,6 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using TodoApi.Data;
 using TodoApi.DTO;
 using TodoApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace TodoApi.Controllers
 {
@@ -11,10 +15,12 @@ namespace TodoApi.Controllers
 	public class UsersController : ControllerBase
 	{
 		private readonly TodoDbContext _context;
+		private readonly IConfiguration _configuration;
 
-		public UsersController(TodoDbContext context)
+		public UsersController(TodoDbContext context, IConfiguration configuration)
 		{
 			_context = context;
+			_configuration = configuration;
 		}
 
 		// Register user
@@ -55,8 +61,8 @@ namespace TodoApi.Controllers
 				return Unauthorized("Invalid username or password");
 			}
 
-			// Return User ID and Username
-			return Ok(new { user.Id, user.Username });
+			var token = GenerateJwtToken(user);
+			return Ok(new { token });
 		}
 
 		// Get all users
@@ -64,6 +70,28 @@ namespace TodoApi.Controllers
 		public async Task<ActionResult<IEnumerable<User>>> GetUsers()
 		{
 			return await _context.Users.ToListAsync();
+		}
+
+		private string GenerateJwtToken(User user)
+		{
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]);
+
+			var tokenDescriptor = new SecurityTokenDescriptor
+			{
+				Subject = new ClaimsIdentity(new[]
+				{
+					new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+					new Claim(ClaimTypes.Name, user.Username)
+				}),
+				Expires = DateTime.UtcNow.AddHours(1),
+				Issuer = _configuration["Jwt:Issuer"],
+				Audience = _configuration["Jwt:Audience"],
+				SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+			};
+
+			var token = tokenHandler.CreateToken(tokenDescriptor);
+			return tokenHandler.WriteToken(token);
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using TodoApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+builder.Services.AddAuthentication(options =>
+{
+	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+	options.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = true,
+		ValidateAudience = true,
+		ValidateLifetime = true,
+		ValidateIssuerSigningKey = true,
+		ValidIssuer = jwtSettings["Issuer"],
+		ValidAudience = jwtSettings["Audience"],
+		IssuerSigningKey = new SymmetricSecurityKey(key)
+	};
+});
 
 // Register DBContext
 builder.Services.AddDbContext<TodoDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -41,6 +66,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors("AllowSpecificOrigin");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
